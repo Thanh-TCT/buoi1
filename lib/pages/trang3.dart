f5b4e75d72dedc559dde38419a5a6af56be02596
Thanh-TCT <trancongthanh032000@gmail.com>
2023-05-16 15:32:29 +0700

    update Trang3

--- lib/pages/trang3.dart
+++ lib/pages/trang3.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:buoi1/pages/trang2.dart';
 import 'package:buoi1/pages/trang4.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -17,256 +18,54 @@ class _Trang3State extends State<Trang3> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.arrow_forward),
+      floatingActionButton : Wrap(
+        spacing: 230,
+        direction: Axis.horizontal,
+        children: [
+          Container(
+            margin : const EdgeInsets.all(10),
+
+      child : FloatingActionButton(
+        
+        heroTag: 'a3',
+        child: const Icon(Icons.arrow_left_sharp),
+        onPressed: (){
+      Navigator .push(context, 
+      MaterialPageRoute(builder:(context) => const Trang2()));
+      },
+      )
+          ),
+      Container(
+      margin: const EdgeInsets.all(10),
+      
+      child : FloatingActionButton(
+        heroTag : 'a4',
+        child: const Icon(Icons.arrow_right_sharp),
         onPressed: (){
       Navigator .push(context, 
       MaterialPageRoute(builder:(context) => const Trang4()));
       },
       ),
-      body : Column(children: [
-        Expanded(
-          child: Row(
-          children: [
-               Expanded(
-                  child: Container (
-                    color: Colors.blue,
-                ),
-            ),
-            SizedBox(width: 10,),
-              Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-              Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-             Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-          ],
-              ),
-        ),
-        SizedBox(height: 10,),
-        Expanded(
-        child: Row(
-          children: [
-               Expanded(
-                  child: Container (
-                    color: Colors.blue,
-                ),
-            ),
-            SizedBox(width: 10,),
-            Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-            Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-            Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            
-            
-          ],
-        ),
+      )
+        ]
       ),
-      SizedBox(height: 10,),
-      Expanded(
-          child: Row(
-          children: [
-               Expanded(
-                  child: Container (
-                    color: Colors.blue,
-                ),
-            ),
-            SizedBox(width: 10,),
-              Expanded(
-              child: Container (
-                color: Colors.blue,
+      body : Container(
+        child: Row (children: [
+          ...List.generate(4, (index) => Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(left: index !=0 ? 10 : 0),
+              child: Column(
+              children: [
+                ... List.generate(8, (index) => Expanded(child: Container(
+                  margin: EdgeInsets.only(top : index !=0 ? 10 : 0),
+                  color: Colors.blue[200],
+                )))
+              ],  
               ),
-            ),
-            SizedBox(width: 10,),
-              Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-             Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-          ],
-              ),
-        ),
-        SizedBox(height: 10,),
-        Expanded(
-          child: Row(
-          children: [
-               Expanded(
-                  child: Container (
-                    color: Colors.blue,
-                ),
-            ),
-            SizedBox(width: 10,),
-              Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-              Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-             Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-          ],
-              ),
-        ),
-        SizedBox(height: 10,),
-        Expanded(
-          child: Row(
-          children: [
-               Expanded(
-                  child: Container (
-                    color: Colors.blue,
-                ),
-            ),
-            SizedBox(width: 10,),
-              Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-              Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-             Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-          ],
-              ),
-        ),
-        SizedBox(height: 10,),
-        Expanded(
-          child: Row(
-          children: [
-               Expanded(
-                  child: Container (
-                    color: Colors.blue,
-                ),
-            ),
-            SizedBox(width: 10,),
-              Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-              Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-             Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-          ],
-              ),
-        ),
-        SizedBox(height: 10,),
-        Expanded(
-          child: Row(
-          children: [
-               Expanded(
-                  child: Container (
-                    color: Colors.blue,
-                ),
-            ),
-            SizedBox(width: 10,),
-              Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-              Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-             Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-          ],
-              ),
-        ),
-        SizedBox(height: 10,),
-        Expanded(
-          child: Row(
-          children: [
-               Expanded(
-                  child: Container (
-                    color: Colors.blue,
-                ),
-            ),
-            SizedBox(width: 10,),
-              Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-              Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(width: 10,),
-             Expanded(
-              child: Container (
-                color: Colors.blue,
-              ),
-            ),
-          ],
-              ),
-        ),
-      ],)
+              ) ))
+        ]),
+          
+          )
       
           
           
