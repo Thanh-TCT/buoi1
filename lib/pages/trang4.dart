c42ef1e954deee7da1068310cb43c8751abf2d08
Thanh-TCT <trancongthanh032000@gmail.com>
2023-05-16 15:32:42 +0700

    update Trang4

--- lib/pages/trang4.dart
+++ lib/pages/trang4.dart
@@ -1,13 +1,15 @@
 
 
 
+import 'package:buoi1/pages/trang3.dart';
+import 'package:buoi1/pages/trang5.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class Trang4 extends StatefulWidget {
   const Trang4({super.key});
-  static const name = '/Trang4';
+  static const name = '/Trang4/';
 
   @override
   State<Trang4> createState() => _Trang4State();
@@ -17,6 +19,32 @@ class _Trang4State extends State<Trang4> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      floatingActionButton : Wrap(
+        spacing: 230,
+        direction: Axis.horizontal,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(10),
+      child: FloatingActionButton(
+        heroTag: 'a5',
+        child: Icon(Icons.arrow_left_sharp),
+        onPressed:(){
+          Navigator.push(context, MaterialPageRoute( builder: (context)=> const Trang3 ()));
+        },
+      )
+          ),
+        Container(
+          margin:const  EdgeInsets.all(10),
+      child: FloatingActionButton(
+        heroTag: 'a6',
+        child: Icon(Icons.arrow_right_sharp),
+        onPressed:(){
+          Navigator.push(context, MaterialPageRoute( builder: (context)=> const Trang5 ()));
+        },
+      ),
+        )
+        ]
+      ),
       body: Column(
         children: [
           Container(
